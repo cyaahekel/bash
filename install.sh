0538e36f158d661b410b513803452ac1d1dbf350
CYAAA SUKA CODING😡 <rajaiblisxyz@gmail.com>
2025-02-05 03:23:41 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,11 +6,5 @@ echo -e "\n[info] installing new repository: tur-repo ...\n"
 pkg install tur-repo -y && pkg update -y
 echo -e "\n[info] installing package and dependencies ...\n"
 pkg install git rust libffi libsodium binutils build-essential python3.10 -y
-echo -e "\n[info] cloning github repository -> 'instahack' ...\n"
-git clone https://github.com/termuxhackers-id/instahack
 echo -e "\n[info] installing module and requirements ...\n"
-cd instahack && git pull && pip3.10 install --upgrade pip && pip3.10 install wheel && pip3.10 install -r requirements.txt
-echo -e "\n[info] remove all README.md from data directories ...\n"
-find data/ -name '*.md' -delete
-echo -e "\n[done] running 'ihack.py' to open instahack console ...\n"
-python3.10 ihack.py
+pip3.10 install --upgrade pip && pip3.10 install wheel && pip3.10 install -r requirements.txt
